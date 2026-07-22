198051a67015ec5e598bb088c056e34501afa577
Павел Крестьянников <krestyannikov.p@ad.nordsrub.ru>
2026-07-22 12:07:14 +0300

    Improve mobile project cards

--- index.php
+++ index.php
@@ -459,9 +459,9 @@ body{padding-top:70px}
     </style>
 
     <!-- Async CSS Loading -->
-    <link rel="stylesheet" href="style.css?v=20260625-cases-cta" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="style.css?v=20260722-mobile-project-order" media="print" onload="this.media='all'">
     <noscript>
-        <link rel="stylesheet" href="style.css?v=20260625-cases-cta">
+        <link rel="stylesheet" href="style.css?v=20260722-mobile-project-order">
     </noscript>
     <style>
         :root {
